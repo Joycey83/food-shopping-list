f43cc0d061393071ccb7ea139b66591413747449
Joycey <Jcheung8385@gmail.com>
2023-04-11 18:47:51 +0100

    changed the usestate variable name to food as it throws an error as the name is the same as the data list name.

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from "react";
 import { foodItems } from "./data";
 
 function App() {
-  const [foodItems, setFoodItems] = useState(foodItems);
+  const [food, setFood] = useState(foodItems);
 
   return (
     <div className="App">
-      <h1>REACT</h1>
+      <h1>{food.length} Food items to buy</h1>
     </div>
   );
 }
