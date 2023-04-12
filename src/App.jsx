aa23b7e53acfb0e043cb1912aabe445f6d545d4e
Joycey <Jcheung8385@gmail.com>
2023-04-12 16:44:25 +0100

    I have created a food component

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,6 @@ function App() {
   return (
     <div className="shopping--container">
       <h1>{food.length} Food items to buy</h1>
-      <div className="product--container">
-        <h2>Food Image</h2>
-        <h2>Food name</h2>
-        <p>Price</p>
-      </div>
     </div>
   );
 }
