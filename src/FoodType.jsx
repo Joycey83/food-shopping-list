da18ba1c31d7da05ea2a0673ef2ba43dded139b8
Joycey <Jcheung8385@gmail.com>
2023-04-12 17:42:25 +0100

    Added some CSS to style the food item information, added background color, text aligned and added some width, margin and padding

--- src/FoodType.jsx
+++ src/FoodType.jsx
@@ -2,7 +2,7 @@ const FoodType = ({ image, text, price }) => {
   return (
     <article className="product--container">
       <img src={image} alt={text} className="img" />
-      <div>
+      <div className="product-info">
         <h4>{text}</h4>
         <p>£{price}</p>
       </div>
