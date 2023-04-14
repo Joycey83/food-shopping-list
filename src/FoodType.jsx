b469c156cc02fdce0c682873b7a27570a222858e
Joycey <Jcheung8385@gmail.com>
2023-04-14 18:04:50 +0100

    Added some CSS to the image so that the image is smaller and circular

--- src/FoodType.jsx
+++ src/FoodType.jsx
@@ -3,7 +3,7 @@ import RemoveButton from "./RemoveButton";
 const FoodType = ({ image, text, price }) => {
   return (
     <article className="product--container">
-      <img src={image} alt={text} className="img" />
+      <img src={image} alt={text} className="food-img" />
       <div className="product-info">
         <h4>{text}</h4>
         <p>£{price}</p>
