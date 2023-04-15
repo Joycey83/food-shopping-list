de3e891ea2804d61461259fd2ed038a82e3191dd
Joycey <Jcheung8385@gmail.com>
2023-04-15 17:44:33 +0100

    created app.module.css, added some transition css styles to the product info container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { foodItems } from "./data";
 import FoodList from "./FoodList";
+import styles from "./App.module.css";
 
 function App() {
   const [food, setFood] = useState(foodItems);
 
   return (
-    <div className="shopping--container">
+    <div className={styles["shopping--container"]}>
       <h1>{food.length} Food items to buy</h1>
       <FoodList foodProduct={food} />
       <button className=" btn clear--btn">Clear All Items</button>
