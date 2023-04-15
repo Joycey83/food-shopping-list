9e9d21d161744a01728bb316875d129e4560f706
Joycey <Jcheung8385@gmail.com>
2023-04-15 16:57:32 +0100

    added the product--container class from the css file

--- src/FoodType.jsx
+++ src/FoodType.jsx
@@ -3,7 +3,7 @@ import styles from "./FoodType.module.css";
 
 const FoodType = ({ image, text, price }) => {
   return (
-    <article className="product--container">
+    <article className={styles["product--container"]}>
       <img src={image} alt={text} className="food-img" />
       <div className="product-info">
         <h4>{text}</h4>
